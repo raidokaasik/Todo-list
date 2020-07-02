71822f3cb9090160024532b55bcdfa86d49d8322
raido kaasik <raido.kaasik@outlook.com>
2020-07-02 08:17:45 +0300

    added gitignore and procfile

--- app.js
+++ app.js
@@ -166,6 +166,14 @@
    res.render("about");
  });
 
- app.listen(3000, function() {
-   console.log("Server started on port 3000");
+ let port = process.env.PORT;
+ if (port == null || port == "") {
+   port = 3000;
+ }
+ app.listen(port, function() {
+   console.log("Listening to KUKU raadio 3000");
  });
+
+ // app.listen(3000, function() {
+ //   console.log("Server started on port 3000");
+ // });
